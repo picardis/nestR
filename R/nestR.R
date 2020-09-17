7fff196fa1ea6314542178ebe76611bd739c972e
bsmity13 <bsmity13@gmail.com>
2020-09-17 16:28:36 -0600

    Added citation to ?nestR

--- R/nestR.R
+++ R/nestR.R
@@ -1,6 +1,13 @@
 #' nestR: A package Analysis of movement recursions to detect reproductive events
 #' and estimate their fate in central place foragers.
 #'
+#' Functions in nestR implement an approach first described in our manuscript,
+#' "Analysis of movement recursions to detect reproductive events and estimate
+#' their fate in central place foragers" (Picardi et al. 2020).
+#'
+#' Find it here:
+#' https://doi.org/10.1186/s40462-020-00201-1
+#'
 #' @docType package
 #' @name nestR
 NULL
